a9744ccc139c8a5ea0aab2c0a6ece9cab0fe4ae2
Isaac Elliott <isaace71295@gmail.com>
2021-09-06 09:27:47 +1000

    change ut8 decoding to reduce code size

--- src/Streaming/Chars/ByteString/Utf8.hs
+++ src/Streaming/Chars/ByteString/Utf8.hs
@@ -33,63 +33,60 @@ chr1 n1 bs = (unsafeChr8 n1, bs)
 utf8Error :: a
 utf8Error = error "utf8 encoding error"
 
-chr2 :: Word8 -> ByteString -> (Char, ByteString)
-chr2 (W8# x1#) bs =
-  case ByteString.uncons bs of
-    Nothing -> utf8Error
-    Just (W8# x2#, bs') -> (C# (chr# (z1# +# z2#)), bs')
-      where
-        !y1# = word2Int# x1#
-        !y2# = word2Int# x2#
-        !z1# = uncheckedIShiftL# (y1# -# 0xC0#) 6#
-        !z2# = y2# -# 0x80#
-{-# INLINEABLE chr2 #-}
+chr2 :: Word8 -> Word8 -> ByteString -> (Char, ByteString)
+chr2 (W8# x1#) (W8# x2#) bs =
+  (C# (chr# (z1# +# z2#)), bs)
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !z1# = uncheckedIShiftL# (y1# -# 0xC0#) 6#
+    !z2# = y2# -# 0x80#
+{-# INLINE chr2 #-}
 
-chr3 :: Word8 -> ByteString -> (Char, ByteString)
-chr3 (W8# x1#) bs =
-  case ByteString.uncons bs of
-    Nothing -> utf8Error
-    Just (W8# x2#, bs') ->
-      case ByteString.uncons bs' of
-        Nothing -> utf8Error
-        Just (W8# x3#, bs'') -> (C# (chr# (z1# +# z2# +# z3#)), bs'')
-          where
-            !y1# = word2Int# x1#
-            !y2# = word2Int# x2#
-            !y3# = word2Int# x3#
-            !z1# = uncheckedIShiftL# (y1# -# 0xE0#) 12#
-            !z2# = uncheckedIShiftL# (y2# -# 0x80#) 6#
-            !z3# = y3# -# 0x80#
-{-# INLINEABLE chr3 #-}
+chr3 :: Word8 -> Word8 -> Word8 -> ByteString -> (Char, ByteString)
+chr3 (W8# x1#) (W8# x2#) (W8# x3#) bs =
+  (C# (chr# (z1# +# z2# +# z3#)), bs)
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !y3# = word2Int# x3#
+    !z1# = uncheckedIShiftL# (y1# -# 0xE0#) 12#
+    !z2# = uncheckedIShiftL# (y2# -# 0x80#) 6#
+    !z3# = y3# -# 0x80#
+{-# INLINE chr3 #-}
 
-chr4 :: Word8 -> ByteString -> (Char, ByteString)
-chr4 (W8# x1#) bs =
-  case ByteString.uncons bs of
-    Nothing -> utf8Error
-    Just (W8# x2#, bs') ->
-      case ByteString.uncons bs' of
-        Nothing -> utf8Error
-        Just (W8# x3#, bs'') ->
-          case ByteString.uncons bs'' of
-            Nothing -> utf8Error
-            Just (W8# x4#, bs''') -> (C# (chr# (z1# +# z2# +# z3# +# z4#)), bs''')
-              where
-                !y1# = word2Int# x1#
-                !y2# = word2Int# x2#
-                !y3# = word2Int# x3#
-                !y4# = word2Int# x4#
-                !z1# = uncheckedIShiftL# (y1# -# 0xF0#) 18#
-                !z2# = uncheckedIShiftL# (y2# -# 0x80#) 12#
-                !z3# = uncheckedIShiftL# (y3# -# 0x80#) 6#
-                !z4# = y4# -# 0x80#
-{-# INLINEABLE chr4 #-}
+chr4 :: Word8 -> Word8 -> Word8 -> Word8 -> ByteString -> (Char, ByteString)
+chr4 (W8# x1#) (W8# x2#) (W8# x3#) (W8# x4#) bs =
+  (C# (chr# (z1# +# z2# +# z3# +# z4#)), bs)
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !y3# = word2Int# x3#
+    !y4# = word2Int# x4#
+    !z1# = uncheckedIShiftL# (y1# -# 0xF0#) 18#
+    !z2# = uncheckedIShiftL# (y2# -# 0x80#) 12#
+    !z3# = uncheckedIShiftL# (y3# -# 0x80#) 6#
+    !z4# = y4# -# 0x80#
+{-# INLINE chr4 #-}
 
 decodeChar :: Word8 -> ByteString -> (Char, ByteString)
-decodeChar n1 bs
-  | n1 < 0xC0 = chr1 n1 bs
-  | n1 < 0xE0 = chr2 n1 bs
-  | n1 < 0xF0 = chr3 n1 bs
-  | otherwise = chr4 n1 bs
+decodeChar n1 bs =
+  if n1 < 0xC0
+    then chr1 n1 bs
+    else case ByteString.uncons bs of
+      Nothing -> utf8Error
+      Just (n2, bs') ->
+        if n1 < 0xE0
+          then chr2 n1 n2 bs'
+          else case ByteString.uncons bs' of
+            Nothing -> utf8Error
+            Just (n3, bs'') ->
+              if n1 < 0xF0
+                then chr3 n1 n2 n3 bs''
+                else case ByteString.uncons bs'' of
+                  Nothing -> utf8Error
+                  Just (n4, bs''') ->
+                    chr4 n1 n2 n3 n4 bs'''
 {-# INLINEABLE decodeChar #-}
 
 newtype StreamUtf8 = StreamUtf8 ByteString
